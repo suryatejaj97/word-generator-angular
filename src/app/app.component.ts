248dacd5f9b211699636759c88849d958e22a87a
suryatejaj97 <45379824+suryatejaj97@users.noreply.github.com>
2020-06-25 23:11:34 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import arrayWords from "../utils/words";
+import arrayCountries from "../utils/countries"
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'word-generator';
+
+  words = '';
+  countries = ''
+  limit = 0;
+
+  handleSlideChange(newLimit: number){
+    this.limit = newLimit;
+  }
+
+  generate(){
+    this.words = arrayWords.slice(0, this.limit).join(" ")
+  }
+
+  generateCountries(){
+    this.countries = arrayCountries.slice(0, this.limit).join(" ")
+  }
 }
---
+++ src/utils/countries.ts
@@ -0,0 +1,19 @@
+export default [
+    'India',
+    'Afganisthan',
+    'Pakisthan',
+    'China',
+    'Sri Lanka',
+    'Bangladesh',
+    'Canada',
+    'Denmark',
+    'Democratic Republic of the Congo',
+    'Hungary',
+    'Honduras',
+    'Lithuania',
+    'Malaysia',
+    'Maldives',
+    'Nepal',
+    'New Zealand',
+    'Panama'
+]
